ce022df9e48a64cc1eff306c9768ca8bd0e7a85e
Jordan Wilson Bishop <jwbishop@lanl.gov>
2024-05-31 15:54:51 -0600

    Add unpack file and update moving zip folder

---
+++ examples/data/Bishop_etal2022/unpack.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+echo "Extracting moving.tar.gz"
+tar -zxf moving.tar.gz
+echo "Extracting transient.tar.gz"
+tar -zxf transient.tar.gz
+echo "Extracting noise.tar.gz"
+tar -zxf noise.tar.gz
+echo "Extracting persistent.tar.gz"
+tar -zxf persistent.tar.gz
+echo "Finished"
